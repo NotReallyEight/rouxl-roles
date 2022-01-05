4368248a4295671192816423928719727344c5c3
NotReallyEight <71493060+NotReallyEight@users.noreply.github.com>
2022-01-05 16:24:08 +0000

    Compile: (8e4fe0c) Add ping command

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.command = void 0;
 const Command_1 = require("../utils/Command");
-exports.command = new Command_1.Command("ping", (message) => {
-    void message.channel.send("Pong!");
+exports.command = new Command_1.Command("ping", (message, _args, client) => {
+    const messageReceived = Date.now();
+    void message.channel.send({
+        embeds: [
+            {
+                title: "Pong!",
+                fields: [
+                    {
+                        name: "Latency",
+                        value: `${messageReceived - message.createdTimestamp}ms`,
+                        inline: true,
+                    },
+                    {
+                        name: "WebSocket Latency",
+                        value: `${client.ws.ping}ms`,
+                        inline: true,
+                    },
+                ],
+            },
+        ],
+    });
 });
