8e4fe0c8a4913ab80f88935900e1e12a17908b8d
NotReallyEight <wangzndg@gmail.com>
2022-01-05 17:23:10 +0100

    Add ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,5 +1,24 @@
 import { Command } from "../utils/Command";
 
-export const command = new Command("ping", (message) => {
-	void message.channel.send("Pong!");
+export const command = new Command("ping", (message, _args, client) => {
+	const messageReceived = Date.now();
+	void message.channel.send({
+		embeds: [
+			{
+				title: "Pong!",
+				fields: [
+					{
+						name: "Latency",
+						value: `${messageReceived - message.createdTimestamp}ms`,
+						inline: true,
+					},
+					{
+						name: "WebSocket Latency",
+						value: `${client.ws.ping}ms`,
+						inline: true,
+					},
+				],
+			},
+		],
+	});
 });
